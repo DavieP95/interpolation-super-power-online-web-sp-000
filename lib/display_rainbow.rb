3c0e27de76a641969f7c9598a3fd36ef97f62d0e
David Polanco <dayvflight@gmail.com>
2021-02-22 09:04:30 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,11 @@
 def display_rainbow (colors)# Write your #display_rainbow method here
 
+puts "R:#{colors[0]}, O:#{colors[1]}, Y:#{colors[2]}, G:#{colors[3]}, B:#{colors[4]}, I:#{colors[5]}, V:#{colors[6]}"+"."
 
 
 
 end
+
+#def color_organizer(colors) #should take a random array of colors and try and arrange them alphabetically.
+#end
+#example color_organizer([blue,orange, red, violet, purple, fushia]) --> blue, fushia, orange, purple, violet
